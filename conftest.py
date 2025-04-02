093a6b03f9cf7980c3c2b7d62a732a238449c04d
Ramin Safarov <silent1962@gmail.com>
2025-04-02 14:48:29 +0300

    Lesson 5: Правки, после ревью.

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture()
 def browser_config():
     options = Options()
-    options.add_argument('--headles')
-    options.add_argument('window_size = 1920, 1080')
+    options.add_argument('--headless')
+    options.add_argument('window-size = 1920,1080')
     browser.config.driver_options = options
     browser.config.base_url = "https://demoqa.com"
